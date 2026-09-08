444d3cd11e8dccae4d9f663714a214e538a65721
Adriss123 <84389684+Adriss123@users.noreply.github.com>
2026-09-08 23:54:07 +0200

    Update navbar.js

--- Javascript/navbar.js
+++ Javascript/navbar.js
@@ -1,4 +1,4 @@
-fetch("html/navbar.html")
+fetch("Javascript/navbarJS.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("navbar").innerHTML = data;
